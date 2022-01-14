cdeea3bb91e664a3cf946ede9d1372987f6542bf
Sondre Mæhre <sondre.mahre@no.experis.com>
2022-01-14 14:36:37 +0100

    First commit

---
+++ src/Administrator.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Administrator {
+
+    public static void main(String[] args) {
+        String test = "Hello";
+        HashMap<String,Integer> minMap = new HashMap<>();
+        minMap.put("Sondre", 29);
+        System.out.println(test);
+        System.out.println(minMap.get("Sondre"));
+    }
+}
